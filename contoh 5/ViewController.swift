daf571102d4b7fcbbe9de43632682cc3152225f0
a11-one <a11_one@yahoo.com>
2020-05-14 15:10:34 +0700

    Menambahkan label

---
+++ contoh 5/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  contoh 5
+//
+//  Created by Aldowan Apta Premala on 14/05/20.
+//  Copyright © 2020 Aldowan Apta Premala. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
